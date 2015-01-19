5a19f36294e8a61b26a98a7e0da8030a34b25aa8
ExcelledProducts <papandreavittorio@gmail.com>
2015-01-19 18:14:16 -0500

    auto merge fix

--- src/org/team2168/RobotMap.java
+++ src/org/team2168/RobotMap.java
@@ -1,20 +1,42 @@
 package org.team2168;
+
 /**
  * The RobotMap is a mapping from the ports sensors and actuators are wired into
  * to a variable name. This provides flexibility changing wiring, makes checking
  * the wiring easier and significantly reduces the number of magic numbers
  * floating around.
  */
-public class RobotMap {	
-	// Solenoid Channels 
+public class RobotMap {
+	/*************************************************************************
+	 *                              WIRING MAP
+	 *************************************************************************/
+
+	//PWM Channels/////////////////////////////////////////////////////////////
+	public final static int INTAKE_MOTORS = 0;
+	public final static int DRIVETRAIN_LEFT_MOTORS = 1;
+	public final static int DRIVETRAIN_RIGHT_MOTORS = 2;
+
+
+	//Solenoid Channels////////////////////////////////////////////////////////
 	public final static int INTAKE_DOUBLE_SOLENOID_FORWARD = 0;
 	public final static int INTAKE_DOUBLE_SOLENOID_REVERSE = 1;
-	
-	// PWM Channels
-	public final static int INTAKE_MOTORS = 0;
-	
-	// Digital Input
+
+
+	//Digital IO Channels//////////////////////////////////////////////////////
 	public final static int LEFT_TOTE_SWITCH = 0;
 	public final static int RIGHT_TOTE_SWITCH = 1;
-	
+
+
+	//Analog Input Channels////////////////////////////////////////////////////
+
+
+
+	//Joysticks////////////////////////////////////////////////////////////////
+	public final static int DRIVER_JOYSTICK = 1;
+	public final static int OPERATOR_JOYSTICK = 2;
+
+
+	/*************************************************************************
+	 *                         DRIVETRAIN PARAMETERS
+	 *************************************************************************/
 }
