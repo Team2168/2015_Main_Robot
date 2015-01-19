930178100a1e4440564607b59a35d04a118f2304
James <james@team2168.org>
2015-01-18 20:48:05 -0500

    Revised intake subsystem to make it a singleton. Changed actuate to engage inn method name for clarity. Some style changes.
    Updated classpath to target java 1.8.

--- src/org/team2168/subsystems/Intake.java
+++ src/org/team2168/subsystems/Intake.java
@@ -8,20 +8,34 @@ import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj.Talon;
 
 /**
- * Intake subystem controlls the intake motors and solenoids. 
+ * The intake subsytem controls the intake motors and solenoids. 
  * @author Vittorio Papandrea
  */
 public class Intake extends Subsystem {
 	
-	DoubleSolenoid rightLeftIntake;
-
-	Talon rightLeftMotor;
+	private static Intake instance = null;
+	private DoubleSolenoid rightLeftIntake;
+	private Talon rightLeftMotor;
 	
-	public Intake() {
+	/**
+	 * A private constructor to prevent multiple instances of the subsystem
+	 * from being created.
+	 */
+	private Intake() {
 		rightLeftIntake = new DoubleSolenoid(RobotMap.INTAKE_DOUBLE_SOLENOID_FORWARD, 
-										 	 	RobotMap.INTAKE_DOUBLE_SOLENOID_REVERSE);
+												RobotMap.INTAKE_DOUBLE_SOLENOID_REVERSE);
 		rightLeftMotor 	= new Talon(RobotMap.INTAKE_MOTORS);
+	}
+	
+	/**
+	 * @return the instance of the subsystem
+	 */
+	public static Intake getInstance() {
+		if(instance == null) {
+			instance = new Intake();
+		}
 		
+		return instance;
 	}
 	
 	/**
@@ -34,7 +48,7 @@ public class Intake extends Subsystem {
 	/**
 	 * Actuates the intake into intaking position
 	 */
-	public void actuateIntake() {
+	public void engageIntake() {
 		rightLeftIntake.set(Value.kForward);
 	}
 	
@@ -60,15 +74,26 @@ public class Intake extends Subsystem {
 	}
 	
 	/**
-	 * Sets the intake Speed of the motors. 
+	 * Sets the intake Speed of the motors.
 	 * @param speed 1 to 0 Tote In. 0 - -1 Tote Out
 	 */
 	public void setIntakeSpeed(double speed) {	
 		rightLeftMotor.set(speed);
 	}
+
+	/**
+	 * Check if there's soemthing in the intake.
+	 * @return true when an object is in the intake.
+	 */
+	public Boolean isTotePresent() {
+		//TODO: tie this to sensors within the intake.
+		return false;
+	}
 	
+	/**
+	 * Set the default command for the subsystem
+	 */
     public void initDefaultCommand() {
-        // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
     }
     
