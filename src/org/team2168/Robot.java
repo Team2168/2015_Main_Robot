2888333ccd431aa411fcacafdf239449b8095eda
James <james@team2168.org>
2015-01-18 22:16:53 -0500

    Added stubs for winch and lift subsystem.

--- src/org/team2168/Robot.java
+++ src/org/team2168/Robot.java
@@ -1,13 +1,15 @@
 
 package org.team2168;
 
+import org.team2168.subsystems.Drivetrain;
+import org.team2168.subsystems.Intake;
+import org.team2168.subsystems.Lift;
+import org.team2168.subsystems.Winch;
+
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
-
-import org.team2168.commands.*;
-import org.team2168.subsystems.*;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -19,27 +21,29 @@ import org.team2168.subsystems.*;
 public class Robot extends IterativeRobot {
 
 	public static OI oi;
-	
+
 	//Subsystem objects
 	public static final Drivetrain drivetrain = Drivetrain.getInstance();
 	public static final Intake intake = Intake.getInstance();
-	
-	//Auto command objects
-    Command autonomousCommand;
+	public static final Lift lift = Lift.getInstance();
+	public static final Winch winch = Winch.getInstance();
 
-    /**
-     * This function is run when the robot is first started up and should be
-     * used for any initialization code.
-     */
-    public void robotInit() {
+	//Auto command objects
+	Command autonomousCommand;
+
+	/**
+	 * This function is run when the robot is first started up and should be
+	 * used for any initialization code.
+	 */
+	public void robotInit() {
 		oi = new OI();
-        // instantiate the command used for the autonomous period
-        //autonomousCommand = new ExampleCommand();
-    }
-	
-    /**
-     * This method runs periodically when the robot is disabled
-     */
+		// instantiate the command used for the autonomous period
+		//autonomousCommand = new ExampleCommand();
+	}
+
+	/**
+	 * This method runs periodically when the robot is disabled
+	 */
 	public void disabledPeriodic() {
 		Scheduler.getInstance().run();
 	}
@@ -47,48 +51,52 @@ public class Robot extends IterativeRobot {
 	/**
 	 * This method initializes the autonomous commands
 	 */
-    public void autonomousInit() {
-        // schedule the autonomous command (example)
-        if (autonomousCommand != null) autonomousCommand.start();
-    }
+	public void autonomousInit() {
+		// schedule the autonomous command (example)
+		if (autonomousCommand != null) {
+			autonomousCommand.start();
+		}
+	}
 
-    /**
-     * This function is called periodically during autonomous
-     */
-    public void autonomousPeriodic() {
-        Scheduler.getInstance().run();
-    }
+	/**
+	 * This function is called periodically during autonomous
+	 */
+	public void autonomousPeriodic() {
+		Scheduler.getInstance().run();
+	}
 
-    /**
-     * This method initializes the teleop commands
-     */
-    public void teleopInit() {
+	/**
+	 * This method initializes the teleop commands
+	 */
+	public void teleopInit() {
 		// This makes sure that the autonomous stops running when
-        // teleop starts running. If you want the autonomous to 
-        // continue until interrupted by another command, remove
-        // this line or comment it out.
-        if (autonomousCommand != null) autonomousCommand.cancel();
-    }
+		// teleop starts running. If you want the autonomous to
+		// continue until interrupted by another command, remove
+		// this line or comment it out.
+		if (autonomousCommand != null) {
+			autonomousCommand.cancel();
+		}
+	}
 
-    /**
-     * This function is called when the disabled button is hit.
-     * You can use it to reset subsystems before shutting down.
-     */
-    public void disabledInit(){
+	/**
+	 * This function is called when the disabled button is hit.
+	 * You can use it to reset subsystems before shutting down.
+	 */
+	public void disabledInit(){
 
-    }
+	}
 
-    /**
-     * This function is called periodically during operator control
-     */
-    public void teleopPeriodic() {
-        Scheduler.getInstance().run();
-    }
-    
-    /**
-     * This function is called periodically during test mode
-     */
-    public void testPeriodic() {
-        LiveWindow.run();
-    }
+	/**
+	 * This function is called periodically during operator control
+	 */
+	public void teleopPeriodic() {
+		Scheduler.getInstance().run();
+	}
+
+	/**
+	 * This function is called periodically during test mode
+	 */
+	public void testPeriodic() {
+		LiveWindow.run();
+	}
 }
