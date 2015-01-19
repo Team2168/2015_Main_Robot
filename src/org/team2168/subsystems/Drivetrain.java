381802fce5abb22383da9576c527d9f933c54d51
James <james@team2168.org>
2015-01-18 21:46:33 -0500

    Removed a potato. Stubbed out some methods for position and heading. Changed to singleton class.

--- src/org/team2168/subsystems/Drivetrain.java
+++ src/org/team2168/subsystems/Drivetrain.java
@@ -7,63 +7,122 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.Talon;
 
 /**
- *
+ * The drivetrain subsystem.
  */
 public class Drivetrain extends Subsystem {
-    
-    // Put methods for controlling this subsystem
-    // here. Call these from Commands.
-	
-	Talon leftMotor;
-	Talon rightMotor;
+
+	private static Drivetrain instance = null;
+	private Talon leftMotor;
+	private Talon rightMotor;
 	
 	/**
-	 * This method instantiates the motors
+	 * This method instantiates the motors.
+	 * Private to prevent creating more than one instance of this subsystem.
 	 */
-	public Drivetrain(){
-		
+	private Drivetrain() {
 		leftMotor = new Talon(RobotMap.DRIVETRAIN_LEFT_MOTORS);
 		rightMotor = new Talon(RobotMap.DRIVETRAIN_LEFT_MOTORS);
-		
 	}
 
+	/**
+	 * @return an instance of the subsystem.
+	 */
+	public static Drivetrain getInstance() {
+		if(instance == null) {
+			instance = new Drivetrain();
+		}
+		
+		return instance;
+	}
+	
 	/**
 	 * This method sets the default command so it always drives with the joysticks
 	 */
     public void initDefaultCommand() {
-        // Set the default command for a subsystem here.
         setDefaultCommand(new DriveWithJoysticks());
     }
     
     /**
-     * This method drives the left motors in the chassis.
-     * @param leftSpeed the speed to drive the motor (speeds -1 to 1, positive is forward, negative is backwards)
+     * Drive the left motors in the chassis.
+     * @param leftSpeed the speed to drive the motor (-1 to 1, positive is forward, negative is backwards)
      */
-    public void driveLeft(double leftSpeed){
+    public void driveLeft(double leftSpeed) {
     	leftMotor.set(leftSpeed);
     }
     
     /**
-     * This method drives the right motors in the chassis.
-     * @param rightSpeed the speed to drive the motor (speeds -1 to 1, positive is forward, negative is backwards)
+     * Drive the right motors in the chassis.
+     * @param rightSpeed the speed to drive the motor (-1 to 1, positive is forward, negative is backwards)
      */
-    public void driveRight(double rightSpeed){
+    public void driveRight(double rightSpeed) {
     	rightMotor.set(rightSpeed);
     }
     
     /**
-     * This method drives both motors in the chassis.
-     * @param leftSpeed the speed to drive the left motor (speeds -1 to 1, positive is forward, negative is backwards)
-     * @param rightSpeed the speed to drive the right motor (speeds -1 to 1, positive is forward, negative is backwards)
+     * Drive both motors in the chassis. (-1 to 1, positive is forward, negative is backwards)
+     * @param leftSpeed the speed to drive the left motor
+     * @param rightSpeed the speed to drive the right motor
      */
-    public void tankDrive(double leftSpeed, double rightSpeed){
+    public void tankDrive(double leftSpeed, double rightSpeed) {
     	leftMotor.set(leftSpeed);
     	rightMotor.set(rightSpeed);
     }
-    
-    public void driveStop(){
+
+    /**
+     * Stop driving the wheels.
+     */
+    public void stop() {
     	leftMotor.set(0);
     	rightMotor.set(0);
+    }
+    
+    /**
+     * Get the distance traveled by the left wheels.
+     * @return distance traveled in inches.
+     */
+    public double getLeftPosition() {
+    	//TODO: return position of left wheels from encoders
+    	return 0.0;
+    }
+    
+    /**
+     * Get the distance traveled by the right wheels.
+     * @return distance traveled in inches
+     */
+    public double getRightPosition() {
+    	//TODO: return position of right wheels from encoders
+    	return 0.0;
+    }
+    
+    /**
+     * Get the distance traveled by the chassis.
+     * @return the average distance in inches traveled by the left and right wheels
+     */
+    public double getPosition() {
+    	return (getLeftPosition() + getRightPosition()) / 2.0;
+    }
+    
+    /**
+     * Zero the distance traveled by the chassis
+     */
+    public void resetPosition() {
+    	//TODO: reset distance traveled by the wheels to zero 
+    }
+    
+    /**
+     * Get the direction the chassis is heading.
+     * @return heading in degrees.
+     */
+    public double getHeading() {
+    	//TODO: return current heading from gyro
+    	return 0.0;
+    }
+    
+    /**
+     * Reset robot heading to zero.
+     */
+    public void resetGyro() {
+    	//TODO: reset t he gyro heading to zero
     }
 }
 
