354ddadfaed9e5dec6c9a672a9d36bd2d2592ee1
Benmw99 <benmw10@gmail.com>
2015-01-22 19:16:15 -0500

    These didn't get shoved in with the last commit

---
+++ src/org/team2168/PIDController/sensors/PIDSensorInterface.java
@@ -0,0 +1,28 @@
+package org.team2168.PIDController.sensors;
+
+public interface PIDSensorInterface {
+	/**
+	 * 
+	 * @return the current rate of the sensor in nominal units of the sensor.
+	 *         Remember when using this method with a PID controller to ensure
+	 *         that the setPoints of the PID controller are in the same unit as
+	 *         returned by this function.
+	 */
+	public double getRate();
+
+	/**
+	 * Resets the rate of the sensor to zero and clears any
+	 * accumulators/counters to zero.
+	 */
+	public void reset();
+
+	/**
+	 * 
+	 * @return the current Position of the sensor in nominal units of the
+	 *         sensor. Remember when using this method with a PID controller to
+	 *         ensure that the setPoints of the PID controller are in the same
+	 *         unit as returned by this function.
+	 */
+	public double getPos();
+
+}
