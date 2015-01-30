5678a5b71a49ef73efa53a7e8dc4b1505f115343
Dustin Garner <frowning.frogs99@gmail.com>
2015-01-30 07:26:14 -0500

    Update EngageIntake.java
    moved runIntakeIn to Initialize

--- src/org/team2168/commands/intake/EngageIntake.java
+++ src/org/team2168/commands/intake/EngageIntake.java
@@ -17,11 +17,11 @@ public class EngageIntake extends Command {
 
     // Called just before this Command runs the first time
     protected void initialize() {
+        Robot.intake.runIntakeIn();
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	Robot.intake.runIntakeIn();
     	Robot.intake.engageIntake();
     }
 
