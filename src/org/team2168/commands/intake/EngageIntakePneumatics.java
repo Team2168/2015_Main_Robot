fc261c48dde8e47f6891be21a7fee16bf79d3db7
Dustin Garner <frowning.frogs99@gmail.com>
2015-01-30 10:57:27 -0500

    Update and rename EngageIntake.java to EngageIntakePneumatics.java
    renamed to make more sense and removed the command to control the intake wheels

--- src/org/team2168/commands/intake/EngageIntakePneumatics.java
+++ src/org/team2168/commands/intake/EngageIntakePneumatics.java
@@ -17,7 +17,6 @@ public class EngageIntake extends Command {
 
     // Called just before this Command runs the first time
     protected void initialize() {
-        Robot.intake.runIntakeIn();
     }
 
     // Called repeatedly when this Command is scheduled to run
