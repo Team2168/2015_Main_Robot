a81b5c34f5db3cf64232ecdf7eb143d3213ba77c
Dustin Garner <frowning.frogs99@gmail.com>
2015-01-30 10:56:26 -0500

    Update and rename DisengageIntake.java to DisengageIntakePneumatics.java
    renamed to make more sense and removed the command for the wheels

--- src/org/team2168/commands/intake/DisengageIntakePneumatics.java
+++ src/org/team2168/commands/intake/DisengageIntakePneumatics.java
@@ -17,7 +17,6 @@ public class DisengageIntake extends Command {
 
     // Called just before this Command runs the first time
     protected void initialize() {
-        Robot.intake.stopIntake();
     }
 
     // Called repeatedly when this Command is scheduled to run
