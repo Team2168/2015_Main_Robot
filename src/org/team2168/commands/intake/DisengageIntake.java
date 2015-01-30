8204b16fc948edc7d8732ef62315c231666550cc
Dustin Garner <frowning.frogs99@gmail.com>
2015-01-30 07:24:56 -0500

    Update DisengageIntake.java
    moved the stopIntake to Initialize

--- src/org/team2168/commands/intake/DisengageIntake.java
+++ src/org/team2168/commands/intake/DisengageIntake.java
@@ -17,11 +17,11 @@ public class DisengageIntake extends Command {
 
     // Called just before this Command runs the first time
     protected void initialize() {
+        Robot.intake.stopIntake();
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	Robot.intake.stopIntake();
     	Robot.intake.releaseIntake();
     }
 
