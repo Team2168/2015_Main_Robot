5df71508993a1fb2ba2f309ee8d04b796846dbcf
Benmw99 <benmw10@gmail.com>
2015-02-03 19:43:58 -0500

    added command to run intake with joystick

--- src/org/team2168/commands/intake/RunIntakeWithJoysticks.java
+++ src/org/team2168/commands/intake/RunIntakeWithJoysticks.java
@@ -1,15 +1,16 @@
 package org.team2168.commands.intake;
 
+import org.team2168.OI;
 import org.team2168.Robot;
 
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
- * This command runs the intake out
+ *
  */
-public class RunIntakeOut extends Command {
+public class RunIntakeWithJoysticks extends Command {
 
-    public RunIntakeOut() {
+    public RunIntakeWithJoysticks() {
         requires(Robot.intake);
     }
 
@@ -19,11 +20,10 @@ public class RunIntakeOut extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	Robot.intake.setIntakeSpeed(-1);
+    	Robot.intake.setIntakeSpeed(OI.operatorJoystick.getLeftStickRaw_Y());
     }
 
     // Make this return true when this Command no longer needs to run execute()
-    //TODO figure out how to find out when the tote is out
     protected boolean isFinished() {
         return false;
     }
