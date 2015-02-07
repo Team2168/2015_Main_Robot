67f3183f5596acffd850a876afa76e0c6d3b55ba
dustin-garner <frowning.frogs99@gmail.com>
2015-02-06 19:46:00 -0500

    cleaned up OI a bit

--- src/org/team2168/OI.java
+++ src/org/team2168/OI.java
@@ -17,26 +17,6 @@ import org.team2168.gamepads.F310;
  * interface to the commands and command groups that allow control of the robot.
  */
 public class OI {
-    // // CREATING BUTTONS
-    // One type of button is a joystick button which is any button on a
-    // joystick.
-    // You create one by telling it which joystick it's on and which button
-    // number it is.
-    // Joystick stick = new Joystick(port);
-    // Button button = new JoystickButton(stick, buttonNumber);
-
-    // There are a few additional built in buttons you can use. Additionally,
-    // by subclassing Button you can create custom triggers and bind those to
-    // commands the same as any other Button.
-
-    // // TRIGGERING COMMANDS WITH BUTTONS
-    // Once you have a button, it's trivial to bind it to a button in one of
-    // three ways:
-
-    // Start the command when the button is pressed and let it run the command
-    // until it is finished as determined by it's isFinished method.
-    // button.whenPressed(new ExampleCommand());
-
     public static F310 driverJoystick;
     public static F310 operatorJoystick;
     public static F310 testJoystickLeft;
@@ -56,6 +36,13 @@ public class OI {
         
         
         
+        //OPERATOR JOYSTICK BUTTON MAP/////////////////////////////////////////////
+        
+        
+        
+        
+        
+        
         //TEST CONTROLLER BUTTON MAP///////////////////////////////////////////////
         testJoystick.ButtonA().whenPressed(new EngageGripper());
         testJoystick.ButtonB().whenPressed(new ReleaseGripper());
@@ -66,13 +53,4 @@ public class OI {
         testJoystick.ButtonLeftStick().whenPressed(new IntakeTote());
         testJoystick.ButtonRightStick().whenPressed(new LiftTote());
     }
-    
-
-    // Run the command while the button is being held down and interrupt it once
-    // the button is released.
-    // button.whileHeld(new ExampleCommand());
-
-    // Start the command when the button is released and let it run the command
-    // until it is finished as determined by it's isFinished method.
-    // button.whenReleased(new ExampleCommand());
 }
