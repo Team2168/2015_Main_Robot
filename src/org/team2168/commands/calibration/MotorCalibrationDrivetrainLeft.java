de0dfb4ecfdfdb0e0592f4c1f75ed71bb314a2df
Harris-Jilani <harrisjilani56@gmail.com>
2015-02-12 20:16:35 -0500

    Changed calibration command (drivetrainLeft) to run one motor controller at a time.

--- src/org/team2168/commands/calibration/MotorCalibrationDrivetrainLeft.java
+++ src/org/team2168/commands/calibration/MotorCalibrationDrivetrainLeft.java
@@ -13,6 +13,9 @@ public class MotorCalibrationDrivetrainLeft extends Command {
 	private double valueSign;
 	private double completeOscillations;
 	private boolean isFinished;
+	private boolean motor1;
+	private boolean motor2;
+	private boolean motor3;
 
     public MotorCalibrationDrivetrainLeft() {
         // Use requires() here to declare subsystem dependencies
@@ -25,26 +28,67 @@ public class MotorCalibrationDrivetrainLeft extends Command {
     	valueSign = 1;
     	completeOscillations = 0;
     	isFinished = false;
+    	motor1 = true;
+    	motor2 = false;
+    	motor3 = false;
     }
 
     /**
      * Oscillates the motors back and forth 3 times for calibration
      */
     protected void execute() {
-    	Robot.drivetrain.driveLeft(oscillatingValue);
-    	oscillatingValue = oscillatingValue + (.05 * valueSign);
-    	if (completeOscillations > 2) {
-    		isFinished = true;
-    		Robot.drivetrain.driveLeft(0);
+    	if (motor1 == true) {
+    		Robot.drivetrain.driveLeft1(oscillatingValue);
+        	oscillatingValue = oscillatingValue + (.05 * valueSign);
+        	if (completeOscillations > 2) {
+        		motor2 = true;
+        		Robot.drivetrain.driveLeft1(0);
+        		motor1 = false;
+        	}
+        	if (oscillatingValue == 1) {
+        		valueSign = -1;
+        	}
+        	if (oscillatingValue == -1) {
+        		valueSign = 1;
+        		completeOscillations = completeOscillations + 1; 
+        	}
+        }
+    	
+    	if (motor2 == true) {
+        	completeOscillations = 0;
+    		Robot.drivetrain.driveLeft2(oscillatingValue);
+    		oscillatingValue = oscillatingValue + (.05 * valueSign);
+    		if (completeOscillations > 2) {
+    			motor3 = true;
+    			Robot.drivetrain.driveLeft2(0);
+    			motor2 = false;
+    		}
+    		if (oscillatingValue == 1) {
+    			valueSign = -1;
+    		}
+    		if (oscillatingValue == -1) {
+    			valueSign = 1;
+    			completeOscillations = completeOscillations + 1;
+    		}
     	}
-    	if (oscillatingValue == 1) {
-    		valueSign = -1;
+    	
+    	if (motor3 == true) {
+    		completeOscillations = 0;
+    		Robot.drivetrain.driveLeft3(oscillatingValue);
+    		oscillatingValue = oscillatingValue + (.05 * valueSign);
+    		if (completeOscillations > 2) {
+    			Robot.drivetrain.driveLeft3(0);
+    			isFinished = true;
+    		}
+    		if (oscillatingValue == 1) {
+    			valueSign = -1;
+    		}
+    		if (oscillatingValue == -1) {
+    			valueSign = 1;
+    			completeOscillations = completeOscillations + 1;
+    		}
     	}
-    	if (oscillatingValue == -1) {
-    		valueSign = 1;
-    		completeOscillations = completeOscillations + 1;
-    	}
-    }
+       }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
