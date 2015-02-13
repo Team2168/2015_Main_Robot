ef5868107a964bc456c468b085f9f82ede7cd77e
Harris-Jilani <harrisjilani56@gmail.com>
2015-02-13 01:22:51 -0500

    Changed motor calibration command (DrivetrainRight) to be able to use each motor controller individually.

--- src/org/team2168/commands/calibration/MotorCalibrationDrivetrainRight.java
+++ src/org/team2168/commands/calibration/MotorCalibrationDrivetrainRight.java
@@ -13,6 +13,9 @@ public class MotorCalibrationDrivetrainRight extends Command {
 	private double valueSign;
 	private double completeOscillations;
 	private boolean isFinished;
+	private boolean motor1;
+	private boolean motor2;
+	private boolean motor3;
 
     public MotorCalibrationDrivetrainRight() {
         // Use requires() here to declare subsystem dependencies
@@ -25,24 +28,65 @@ public class MotorCalibrationDrivetrainRight extends Command {
     	valueSign = 1;
     	completeOscillations = 0;
     	isFinished = false;
+    	motor1 = true;
+    	motor2 = false;
+    	motor3 = false;
     }
 
     /**
      * Oscillates the motors back and forth 3 times for calibration
      */
     protected void execute() {
-    	Robot.drivetrain.driveLeft(oscillatingValue);
-    	oscillatingValue = oscillatingValue + (.05 * valueSign);
-    	if (completeOscillations > 2) {
-    		isFinished = true;
-    		Robot.drivetrain.driveRight(0);
+    	if (motor1 == true) {
+    		Robot.drivetrain.driveRight1(oscillatingValue);
+    		oscillatingValue = oscillatingValue + (.05 * valueSign);
+    		if (completeOscillations > 2) {
+    			motor2 = true;
+    			Robot.drivetrain.driveRight(0);
+    			motor1 = false;
+    		}
+    		if (oscillatingValue == 1) {
+    			valueSign = -1;
+    		}
+    		if (oscillatingValue == -1) {
+    			valueSign = 1;
+    			completeOscillations = completeOscillations + 1;
+    		}
     	}
-    	if (oscillatingValue == 1) {
-    		valueSign = -1;
+    	
+    	if (motor2 == true) {
+    		completeOscillations = 0;
+    		Robot.drivetrain.driveRight2(oscillatingValue);
+    		oscillatingValue = oscillatingValue + (.05 * valueSign);
+    		if (completeOscillations > 2) {
+    			motor3 = true;
+    			Robot.drivetrain.driveRight2(0);
+    			motor2 = false;
+    		}
+    		if (oscillatingValue == 1) {
+    			valueSign = -1;
+    		}
+    		if (oscillatingValue == -1) {
+    			valueSign = 1;
+    			completeOscillations = completeOscillations + 1;
+    		}
     	}
-    	if (oscillatingValue == -1) {
-    		valueSign = 1;
-    		completeOscillations = completeOscillations + 1;
+    	
+    	if (motor3 == true) {
+    		completeOscillations = 0;
+    		Robot.drivetrain.driveRight3(oscillatingValue);
+    		oscillatingValue = oscillatingValue + (.05 * valueSign);
+    		if (completeOscillations > 2) {
+    			Robot.drivetrain.driveRight3(0);
+    			isFinished = true;
+    		}
+    		if (oscillatingValue == 1) {
+    			valueSign = -1;
+    		}
+    		if (oscillatingValue == -1) {
+    			valueSign = 1;
+    			completeOscillations = completeOscillations + 1;
+    		}
     	}
     }
 
