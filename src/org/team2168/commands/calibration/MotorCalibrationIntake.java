00ff6633d4682605f00a05438b96246396147bd6
Harris-Jilani <harrisjilani56@gmail.com>
2015-02-20 18:32:15 -0500

    Changed Intake calibration command to oscillate the value of the right motor, and then the left motor.

--- src/org/team2168/commands/calibration/MotorCalibrationIntake.java
+++ src/org/team2168/commands/calibration/MotorCalibrationIntake.java
@@ -13,6 +13,9 @@ public class MotorCalibrationIntake extends Command {
 	private double valueSign;
 	private double completeOscillations;
 	private boolean isFinished;
+	private boolean rightMotor;
+	private boolean leftMotor;
+	
 
     public MotorCalibrationIntake() {
         // Use requires() here to declare subsystem dependencies
@@ -27,27 +30,48 @@ public class MotorCalibrationIntake extends Command {
     	oscillatingValue = -1;
     	valueSign = 1;
     	completeOscillations = 0;
+    	rightMotor = true;
+    	leftMotor = false;
     	isFinished = false;
-    	
     }
 
     // Called repeatedly when this Command is scheduled to run
     /**
-     * Oscillates the value of the speed of the lift to its maximum and minimum 3 times
+     * Oscillates the value of the speed of the intake to its maximum and minimum value 3 times 
+     * for the left intake motor the right = intake motor, individually.
      */
     protected void execute() {
-    	Robot.intake.setIntakeSpeed(oscillatingValue);
-    	oscillatingValue = oscillatingValue + (.05 * valueSign);
-    	if (completeOscillations > 2) {
-    		isFinished = true;
-    		Robot.intake.setIntakeSpeed(0);
+    	if (rightMotor == true) {
+    		Robot.intake.setRightIntakeSpeed(oscillatingValue);
+    		oscillatingValue = oscillatingValue + (valueSign * .05);
+    		if (completeOscillations > 2) {
+    			leftMotor = true;
+    			Robot.intake.setRightIntakeSpeed(0);
+    			rightMotor = false;
+    		}
+    		if (oscillatingValue == 1) {
+    			valueSign = -1;
+    		}
+    		if (oscillatingValue == -1) {
+    			valueSign = 1;
+    			completeOscillations = completeOscillations + 1;
+    		}
     	}
-    	if (oscillatingValue == 1) {
-    		valueSign = -1;
-    	}
-    	if (oscillatingValue == -1) {
-    		valueSign = 1;
-    		completeOscillations = completeOscillations + 1;
+    	if (leftMotor == true) {
+    		completeOscillations = 0;
+    		Robot.intake.setLeftIntakeSpeed(oscillatingValue);
+    		oscillatingValue = (valueSign * .05);
+    		if (completeOscillations > 2) {
+    			Robot.intake.setLeftIntakeSpeed(0);
+    			leftMotor = false;
+    		}
+    		if (oscillatingValue == 1) {
+    			valueSign = -1;
+    		}
+    		if (oscillatingValue == -1) {
+    			valueSign = 1;
+    			completeOscillations = completeOscillations + 1;
+    		}
     	}
     }
 
