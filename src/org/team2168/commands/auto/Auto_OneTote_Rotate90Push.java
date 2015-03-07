15b075f3142f7ea92b822598f9ddafe567a75149
notincontrol <kevin@team2168.org>
2015-03-07 02:45:29 -0500

    added three tote auto commandgroup

--- src/org/team2168/commands/auto/Auto_OneTote_Rotate90Push.java
+++ src/org/team2168/commands/auto/Auto_OneTote_Rotate90Push.java
@@ -16,22 +16,6 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class Auto_OneTote_Rotate90Push extends CommandGroup {
     
     public  Auto_OneTote_Rotate90Push() {
-        // Add Commands here:
-        // e.g. addSequential(new Command1());
-        //      addSequential(new Command2());
-        // these will run in order.
-
-        // To run multiple commands at the same time,
-        // use addParallel()
-        // e.g. addParallel(new Command1());
-        //      addSequential(new Command2());
-        // Command1 and Command2 will run in parallel.
-
-        // A command group will require all of the subsystems that each member
-        // would require.
-        // e.g. if Command1 requires chassis, and Command2 requires arm,
-        // a CommandGroup containing them would require both the chassis and the
-        // arm.
 
     	//Zeros the lift
     	addSequential(new ZeroLift(),2);
---
+++ src/org/team2168/commands/auto/Auto_ThreeToteStack.java
@@ -0,0 +1,85 @@
+package org.team2168.commands.auto;
+
+import org.team2168.commands.drivetrain.DriveXDistance;
+import org.team2168.commands.drivetrain.RotateXDistance;
+import org.team2168.commands.drivetrain.RotateXDistancePIDZZZ;
+import org.team2168.commands.gripper.EngageGripper;
+import org.team2168.commands.intake.DisengageIntake;
+import org.team2168.commands.intake.EngageIntake;
+import org.team2168.commands.intake.IntakeSingleTote;
+import org.team2168.commands.intake.SetIntakeSpeed;
+import org.team2168.commands.lift.LiftOneTote;
+import org.team2168.commands.lift.ZeroLift;
+import org.team2168.commands.lift.PIDCommands.LiftPIDPosition;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+/**
+ *
+ */
+public class Auto_ThreeToteStack extends CommandGroup {
+    
+    public  Auto_ThreeToteStack() {    	
+    	//Start with first tote in robot
+    	//Intake 1st tote
+    	addParallel(new IntakeSingleTote(),2);
+    	
+    	//Zeros the lift
+    	addSequential(new ZeroLift(),2);
+    	addSequential(new ZeroLift(),2);
+    	addSequential(new EngageGripper(),2);
+    	
+    	//lift 1st tote above can height
+    	addSequential(new DisengageIntake(),2);
+    	addSequential(new LiftPIDPosition(40, 1), 4); // raise 1st tote above garbage can
+    	
+    	
+    	//Start spinning wheels backwards for 1st bin and driving fwd
+    	addSequential(new EngageIntake(),2);
+    	addParallel(new SetIntakeSpeed(1),3);
+    	addSequential(new DriveXDistance(6, 0.2),5); //drive slow to move bin
+    	
+    	//bin is out of way so drive to next tote faster
+    	addParallel(new IntakeSingleTote(),2);
+    	addSequential(new DriveXDistance(6, 0.4),5); //drive slow to move bin
+    	
+    	
+    	//aquired 2nd tote, so now we lift
+    	addSequential(new LiftPIDPosition(1, 0.8), 4); // lower 1st tote onto 2nd tote
+    	addSequential(new ZeroLift(),2);
+    	addSequential(new ZeroLift(),2);
+    	addSequential(new DisengageIntake(),2);
+    	addSequential(new LiftPIDPosition(40, 1), 4); // raise 2nd tote above garbage can
+    	
+    	
+    	//Start spinning wheels backwards for 2nd bin and driving fwd
+    	addSequential(new EngageIntake(),2);
+    	addParallel(new SetIntakeSpeed(1),3);
+    	addSequential(new DriveXDistance(6, 0.2),5); //drive slow to move bin
+    	
+    	//bin is out of way so drive to next tote faster
+    	addParallel(new IntakeSingleTote(),2);
+    	addSequential(new DriveXDistance(6, 0.4),5); //drive slow to move bin
+    	
+    	
+    	//Acquired 3rd tote so just drive to auto zone
+    	addParallel(new SetIntakeSpeed(-0.5),3);
+    	addSequential(new RotateXDistancePIDZZZ(90, 0.4),4);
+    	addSequential(new DriveXDistance(12, 0.5),5); //drive slow with stack
+    	
+    	
+    	//at auto zone, so lets lower stack
+    	addSequential(new LiftPIDPosition(1, 0.8), 4); // lower 2tote stack onto 3rd tote
+    	addSequential(new ZeroLift(),2);
+    	addSequential(new ZeroLift(),2);
+    	
+    	//release set
+    	addSequential(new DisengageIntake(),2);
+    	addSequential(new DriveXDistance(2, 0.5),5); //drive slow with statck
+    	
+    	//DONE
+    	
+
+    	
+    }
+}
