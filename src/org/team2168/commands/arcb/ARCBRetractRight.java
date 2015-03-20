4159e96c039b573a902360c580b935416665ae98
James <james@team2168.org>
2015-03-20 08:39:39 -0700

    Create ARCBRetractRight.java

---
+++ src/org/team2168/commands/arcb/ARCBRetractRight.java
@@ -0,0 +1,38 @@
+Enter file contents herepackage org.team2168.commands.arcb;
+
+import org.team2168.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class ARCBRetractRight extends Command {
+
+    public ARCBRetractRight() {
+    	requires(Robot.binGrabber);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	Robot.binGrabber.retractRight();
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return Robot.binGrabber.isRightRetracted();
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
