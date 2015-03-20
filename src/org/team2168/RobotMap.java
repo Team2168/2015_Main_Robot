afd682e381526b79ab1e477d6c2b050fe1b745c5
James <james@team2168.org>
2015-03-20 08:16:22 -0700

    Update RobotMap.java
    Moved the arcb from a double solenoid to a relay channel.

--- src/org/team2168/RobotMap.java
+++ src/org/team2168/RobotMap.java
@@ -50,8 +50,9 @@ public class RobotMap {
 	public final static int GRIPPER_DOUBLE_SOLENOID_REVERSE = 3;
 	public final static int LIFT_BRAKE_DOUBLE_SOLENOID_FORWARD = 4;
 	public final static int LIFT_BRAKE_DOUBLE_SOLENOID_REVERSE = 5;
-	public final static int PUSHER_SOLENOID_FORWARD = 6;
-	public final static int PUSHER_SOLENOID_REVERSE = 7;
+	
+	//Relay Channels///////////////////////////////////////////////////////////
+	public final static int ARCB_RELAY = 0;
 
 	//Digital IO Channels//////////////////////////////////////////////////////
 	//0-9 on RoboRio 10-25 on MXP
