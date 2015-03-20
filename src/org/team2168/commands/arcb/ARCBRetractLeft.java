c438e50510763941095c7ae570f5bea520f5a06a
James <james@team2168.org>
2015-03-20 08:39:52 -0700

    Update ARCBRetractLeft.java

--- src/org/team2168/commands/arcb/ARCBRetractLeft.java
+++ src/org/team2168/commands/arcb/ARCBRetractLeft.java
@@ -7,7 +7,7 @@ import edu.wpi.first.wpilibj.command.Command;
 /**
  *
  */
-public class ARCBRetract extends Command {
+public class ARCBRetractLeft extends Command {
 
     public ARCBRetractLeft() {
     	requires(Robot.binGrabber);
