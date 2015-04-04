e041f7189658e3b31b6b5e08674aa87ab08ba1ee
notincontrol <kevin@team2168.org>
2015-04-04 12:23:36 -0400

    added driverstation data to log files

--- src/org/team2168/utils/ConsolePrinter.java
+++ src/org/team2168/utils/ConsolePrinter.java
@@ -48,7 +48,7 @@ public class ConsolePrinter {
 			Date date = new Date() ;
 			SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH-mm-ss") ;
 			this.log = new PrintWriter("/home/lvuser/Logs/" + dateFormat.format(date) + "-Log.txt", "UTF-8");
-			log.println("Time \t TimeofDay \t VoltageL1 \t VoltageL2 \t VoltageL3 \t VoltageR1 \t VoltageR2 \t VoltageR3 \t CurrentL1 \t CurrentL2 \t CurrentL3 \t CurrentR1 \t CurrentR2 \t CurrentR3 \t Gyrot SPI Gyro Angle \t SPI Gyro Rate \t Left Encoder Position \t Left Encoder Rate \t Right Encoder Position \t Right Encoder Rate \t Lift Voltage \t Lift Left Current \t Lift Right Current \t Lift Position \t Lift Raw Rate \t Lift Rate \t Accel X \t Accel Y \t Accel Z \t Accel Pitch \t Accel Roll ");
+			log.println("Time \t TimeofDay \t Disabled \t Enabled \t Auto \t Teleop \t FMS \t MatchTime \t Batt Volt \t VoltageL1 \t VoltageL2 \t VoltageL3 \t VoltageR1 \t VoltageR2 \t VoltageR3 \t CurrentL1 \t CurrentL2 \t CurrentL3 \t CurrentR1 \t CurrentR2 \t CurrentR3 \t Gyrot SPI Gyro Angle \t SPI Gyro Rate \t Left Encoder Position \t Left Encoder Rate \t Right Encoder Position \t Right Encoder Rate \t Lift Voltage \t Lift Left Current \t Lift Right Current \t Lift Position \t Lift Raw Rate \t Lift Rate \t Accel X \t Accel Y \t Accel Z \t Accel Pitch \t Accel Roll ");
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -175,6 +175,15 @@ public class ConsolePrinter {
 			//file log
 			log.println(Timer.getFPGATimestamp() + "\t" +
 					System.currentTimeMillis() + "\t" +
+					
+					DriverStation.getInstance().isDisabled() + "\t" +
+					DriverStation.getInstance().isEnabled() + "\t" +
+					DriverStation.getInstance().isAutonomous() + "\t" +
+					DriverStation.getInstance().isOperatorControl() + "\t" +
+					DriverStation.getInstance().isFMSAttached() + "\t" +
+					DriverStation.getInstance().getMatchTime() + "\t" +
+					DriverStation.getInstance().getBatteryVoltage() + "\t" +
+					
 					Robot.drivetrain.getLeft1MotorVoltage() + "\t" +
 					Robot.drivetrain.getLeft2MotorVoltage() + "\t" +
 					Robot.drivetrain.getLeft3MotorVoltage() + "\t" +
