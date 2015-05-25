683d9afbb5024991b4ecfedbe2fb02252ff5d4b5
James <james@team2168.org>
2015-05-25 14:51:03 -0400

    Added another drive command that runs backwards slowly. The fast drive backwards ends up hitting the totes and bouncing off. Adding a parallel drive command moves us back while the bin rippers are lowering. Worked consistently after this change at BattleCry16.

--- src/org/team2168/commands/auto/NEChamps/Auto_RCCB_Fast.java
+++ src/org/team2168/commands/auto/NEChamps/Auto_RCCB_Fast.java
@@ -2,23 +2,6 @@ package org.team2168.commands.auto.NEChamps;
 
 import org.team2168.commands.Sleep;
 import org.team2168.commands.drivetrain.DriveXDistance;
-import org.team2168.commands.drivetrain.DriveXDistanceUntilObject;
-import org.team2168.commands.drivetrain.RotateAboutRightWheel;
-import org.team2168.commands.drivetrain.RotateXDistance;
-import org.team2168.commands.drivetrain.RotateXDistancePIDZZZ;
-import org.team2168.commands.gripper.EngageGripper;
-import org.team2168.commands.gripper.ReleaseGripper;
-import org.team2168.commands.intake.DisengageIntake;
-import org.team2168.commands.intake.DriveIntakeWheelIndependt;
-import org.team2168.commands.intake.EngageIntake;
-import org.team2168.commands.intake.IntakeSingleTote;
-import org.team2168.commands.intake.SetIntakeSpeed;
-import org.team2168.commands.intake.StopIntakeWheels;
-import org.team2168.commands.intake.WaitForIntakeToClearObject;
-import org.team2168.commands.intake.WaitForObjectInIntake;
-import org.team2168.commands.lift.LiftOneTote;
-import org.team2168.commands.lift.ZeroLift;
-import org.team2168.commands.lift.PIDCommands.LiftPIDPosition;
 import org.team2168.commands.rccb.DeployRCCB;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
@@ -29,15 +12,15 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
  *placed holding 1 tote, and that the 2nd bin is removed.. This only removes the First Bin
  */
 public class Auto_RCCB_Fast extends CommandGroup {
-    
-    public  Auto_RCCB_Fast() { 
-    	
-    	addSequential(new DriveXDistance(-8, 1, 0.2), 0.38); //hight setpoint for full speed drive
-    	addSequential(new Sleep(),0.1);
-    	addSequential(new DeployRCCB());
-    	addSequential(new Sleep(), 0.7);
-    	addSequential(new DriveXDistance(8, 1,0.5),2);
-    	
-   	
-    }
+
+	public  Auto_RCCB_Fast() {
+
+		addSequential(new DriveXDistance(-8, 1, 0.2), 0.38); //hight setpoint for full speed drive
+		addParallel(new DriveXDistance(-1, 0.5), 2); // This is here to counter act the forward motion - gets canceled when driving forward.
+		//addSequential(new Sleep(),0.1);
+		addSequential(new DeployRCCB());
+		addSequential(new Sleep(), 0.7);
+		addSequential(new DriveXDistance(8, 1,0.5),2);
+
+	}
 }
